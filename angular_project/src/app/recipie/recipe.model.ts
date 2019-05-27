df1c88e7120f881e10c9d062bdd68dfdbd8aedb6
madanu mary kavya <krish13132013@gmail.com>
2019-05-27 11:13:15 +0530

    adding recipie model

---
+++ angular_project/src/app/recipie/recipe.model.ts
@@ -0,0 +1,11 @@
+
+export class recepie{
+    public recipeName : string;
+    public recipeDesc : string;
+    public image:string; 
+constructor(name,desc,img){
+    this.recipeName=name
+    this.recipeDesc=desc;
+    this.image=img    
+}
+}
